a32310e10a2377e93b94e1839319b3e4a8d80e50
binstock <binstock@dee37da4-d874-0410-bba0-bb64538a29bc>
2009-10-03 01:00:17 +0000

    Initial commit

---
+++ trunk/src/test/groovy/runTests.bat
@@ -0,0 +1,2 @@
+rem runs the functional tests in scripts.txt with output going to the <project> jar directory
+groovy framework.groovy scripts.txt d:\dev\platypus\platypus\target\jar
---
+++ trunk/src/test/groovy/test1.bat
@@ -0,0 +1,1 @@
+java -jar D:\Dev\platypus\platypus\target\jar\Platypus.jar  D:\Dev\platypus\testdata\commandonly.platy d:\dev\platypus\platypus\target\jar\shouldBeEmpty.pdf
