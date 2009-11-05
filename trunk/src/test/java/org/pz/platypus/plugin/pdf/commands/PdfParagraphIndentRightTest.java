e8e8610e296e57d6bab3999f38b064116d7cc414
binstock <binstock@dee37da4-d874-0410-bba0-bb64538a29bc>
2009-11-05 10:31:03 +0000

    Refactored PdfParagraphIndentRight and added unit tests, did some minor touch ups to test for PdfParagraphIndent.

--- trunk/src/test/java/org/pz/platypus/plugin/pdf/commands/PdfParagraphIndentRightTest.java
+++ trunk/src/test/java/org/pz/platypus/plugin/pdf/commands/PdfParagraphIndentRightTest.java
@@ -18,11 +18,11 @@ import org.pz.platypus.test.mocks.MockLiterals;
 import java.util.logging.Level;
 
 /**
- * Test processing of changing paragraph indent.
+ * Test processing of changing paragraph indent from the right side.
  *
  * @author alb
  */
-public class PdfParagraphIndentTest
+public class PdfParagraphIndentRightTest
 {
     private GDD gdd;
     private PdfData pdd;
@@ -50,12 +50,13 @@ public class PdfParagraphIndentTest
         parm = new CommandParameter();
         parm.setAmount( currColWidth / 4.0f );
         parm.setUnit( UnitType.POINT );
-        tok = new Token( new Source(), TokenType.COMMAND, "[paraindent:", "[paraident:200]", parm );
+        tok = new Token( new Source(), TokenType.COMMAND, "[paraindentR:",
+                         "[paraidentR:200]", parm );
 
-        PdfParagraphIndent ppi = new PdfParagraphIndent();
-        ppi.process( pdd, tok, 2 );
+        PdfParagraphIndentRight ppir = new PdfParagraphIndentRight();
+        ppir.process( pdd, tok, 2 );
 
-        assertEquals( currColWidth / 4f, pdd.getParagraphIndent(), 0.5f );
+        assertEquals( currColWidth / 4f, pdd.getParagraphIndentRight(), 0.5f );
     }
 
     @Test
@@ -68,12 +69,12 @@ public class PdfParagraphIndentTest
         parm = new CommandParameter();
         parm.setAmount( 0f );
         parm.setUnit( UnitType.POINT );
-        tok = new Token( new Source(), TokenType.COMMAND, "[paraindent:", "[paraident:0]", parm );
+        tok = new Token( new Source(), TokenType.COMMAND, "[paraindentR:", "[paraidentR:0]", parm );
 
-        PdfParagraphIndent ppi = new PdfParagraphIndent();
-        ppi.process( pdd, tok, 2 );
+        PdfParagraphIndentRight ppir = new PdfParagraphIndentRight();
+        ppir.process( pdd, tok, 2 );
 
-        assertEquals( 0f, pdd.getParagraphIndent(), 0.1f );
+        assertEquals( 0f, pdd.getParagraphIndentRight(), 0.1f );
     }
 
     @Test
@@ -87,13 +88,14 @@ public class PdfParagraphIndentTest
         parm = new CommandParameter();
         parm.setAmount( -4.0f );
         parm.setUnit( UnitType.POINT );
-        tok = new Token( new Source(), TokenType.COMMAND, "[paraindent:", "[paraident:-4]", parm );
+        tok = new Token( new Source(), TokenType.COMMAND, "[paraindentR:",
+                         "[paraidentR:-4]", parm );
 
-        PdfParagraphIndent ppi = new PdfParagraphIndent();
-        ppi.process( pdd, tok, 2 );
+        PdfParagraphIndentRight ppir = new PdfParagraphIndentRight();
+        ppir.process( pdd, tok, 2 );
 
         // should result in no change to current indent value
-        assertEquals( startingIndent, pdd.getParagraphIndent(), 0.1f );
+        assertEquals( startingIndent, pdd.getParagraphIndentRight(), 0.1f );
 
         MockLogger ml = (MockLogger) gdd.getLogger();
         assertTrue( ml.getMessage().contains( "ERROR.INVALID_PARAGRAPH_INDENT" ));
@@ -110,14 +112,14 @@ public class PdfParagraphIndentTest
         parm = new CommandParameter();
         parm.setAmount( 500 *  currColWidth );
         parm.setUnit( UnitType.POINT );
-        tok = new Token( new Source(), TokenType.COMMAND, "[paraindent:",
-                         "[paraident:144000]", parm );
+        tok = new Token( new Source(), TokenType.COMMAND, "[paraindentR:",
+                         "[paraidentR:144000]", parm );
 
-        PdfParagraphIndent ppi = new PdfParagraphIndent();
-        ppi.process( pdd, tok, 2 );
+        PdfParagraphIndentRight ppir = new PdfParagraphIndentRight();
+        ppir.process( pdd, tok, 2 );
 
         // should result in no change to current indent value
-        assertEquals( startingIndent, pdd.getParagraphIndent(), 0.1f );
+        assertEquals( startingIndent, pdd.getParagraphIndentRight(), 0.5f );
 
         MockLogger ml = (MockLogger) gdd.getLogger();
         assertTrue( ml.getMessage().contains( "ERROR.INVALID_PARAGRAPH_INDENT" ));
