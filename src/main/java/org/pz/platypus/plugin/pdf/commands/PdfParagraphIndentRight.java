ef670d3171005f52f6c76b049e8d554ed0fad6a9
binstock <binstock@dee37da4-d874-0410-bba0-bb64538a29bc>
2009-11-05 10:31:03 +0000

    Refactored PdfParagraphIndentRight and added unit tests, did some minor touch ups to test for PdfParagraphIndent.

--- src/main/java/org/pz/platypus/plugin/pdf/commands/PdfParagraphIndentRight.java
+++ src/main/java/org/pz/platypus/plugin/pdf/commands/PdfParagraphIndentRight.java
@@ -1,7 +1,7 @@
 /***
  *  Platypus: Page Layout and Typesetting Software (free at platypus.pz.org)
  *
- *  Platypus is (c) Copyright 2006-08 Pacific Data Works LLC. All Rights Reserved.
+ *  Platypus is (c) Copyright 2006-09 Pacific Data Works LLC. All Rights Reserved.
  *  Licensed under Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0.html)
  */
 
@@ -15,7 +15,7 @@ import org.pz.platypus.plugin.pdf.Conversions;
 import org.pz.platypus.plugin.pdf.PdfData;
 
 /**
- * Implementation of indentation of a full paragraph for PDF plugin
+ * Implementation of indentation of a full paragraph from the right side for PDF plugin
  *
  * @author alb
  */
@@ -30,22 +30,37 @@ public class PdfParagraphIndentRight implements OutputCommandable
         }
 
         PdfData pdf = (PdfData) context;
-
-        float indent = Conversions.convertParameterToPoints( tok.getParameter(), pdf );
-        if ( indent < 0 ) { //TODO: get column width and make it the maximum indent to test for
-            GDD gdd = pdf.getGdd();
-            gdd.logWarning( gdd.getLit( "LINE#" ) + ": " + tok.getSource().getLineNumber() + " " +
-                            gdd.getLit( "ERROR.INVALID_PARAGRAPH_INDENT" ) + " " +
-                            gdd.getLit( "IGNORED" ));
+        float indent = obtainNewIndent( tok, pdf );
+        if( Float.isNaN( indent )) {
             return;
         }
 
-        float currParaIndent = pdf.getParagraphIndentRight();
-
-        if ( indent != currParaIndent ) {
+        float currParaIndentRight = pdf.getParagraphIndentRight();
+        if ( indent != currParaIndentRight ) {
             pdf.setParagraphIndentRight( indent, tok.getSource() );
             return;
         }
+    }
+
+    /**
+     * Extracts the new indent amount from the token and verifies its value.
+     * @param tok token containing the new indent specification
+     * @param pdf current PDF status info
+     * @return the indent if value is valid; otherwise Float.Nan (Nan = a value that's Not a Number)
+     */
+    float obtainNewIndent( final Token tok, final PdfData pdf )
+    {
+        float indent = Conversions.convertParameterToPoints( tok.getParameter(), pdf );
+        if ( indent < 0 || indent >= pdf.getColumnWidth() ) {
+            GDD gdd = pdf.getGdd();
+            gdd.logWarning( gdd.getLit( "FILE#" ) + ": " + tok.getSource().getFileNumber() + " " +
+                            gdd.getLit( "LINE#" ) + ": " + tok.getSource().getLineNumber() + " " +
+                            gdd.getLit( "ERROR.INVALID_PARAGRAPH_INDENT" ) + " " +
+                            gdd.getLit( "IGNORED" ));
+            return( Float.NaN );
+        }
+
+        return( indent );
     }
 
     public String getRoot()
