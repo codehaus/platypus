47ca9b6766b1ac94ee058c7d3ac1510161208d2f
binstock <binstock@dee37da4-d874-0410-bba0-bb64538a29bc>
2009-11-06 03:32:19 +0000

    Refactored PdfFirstLineIndent and added unit tests for it.

--- src/main/java/org/pz/platypus/plugin/pdf/commands/PdfFirstLineIndent.java
+++ src/main/java/org/pz/platypus/plugin/pdf/commands/PdfFirstLineIndent.java
@@ -1,7 +1,7 @@
 /***
  *  Platypus: Page Layout and Typesetting Software (free at platypus.pz.org)
  *
- *  Platypus is (c) Copyright 2006-08 Pacific Data Works LLC. All Rights Reserved.
+ *  Platypus is (c) Copyright 2006-09 Pacific Data Works LLC. All Rights Reserved.
  *  Licensed under Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0.html)
  */
 
@@ -9,13 +9,14 @@ package org.pz.platypus.plugin.pdf.commands;
 
 import org.pz.platypus.GDD;
 import org.pz.platypus.Token;
+import org.pz.platypus.exceptions.InvalidCommandParameterException;
 import org.pz.platypus.interfaces.OutputCommandable;
 import org.pz.platypus.interfaces.OutputContextable;
 import org.pz.platypus.plugin.pdf.Conversions;
 import org.pz.platypus.plugin.pdf.PdfData;
 
 /**
- * Implementation of indentation of a full paragraph for PDF plugin
+ * Implementation of indentation of the first line of a paragraph for PDF plugin
  *
  * @author alb
  */
@@ -25,27 +26,59 @@ public class PdfFirstLineIndent implements OutputCommandable
 
     public void process( final OutputContextable context, final Token tok, final int tokNum )
     {
+        float newIndent;
+
         if( context == null || tok == null ) {
             throw new IllegalArgumentException();
         }
 
         PdfData pdf = (PdfData) context;
 
-        float indent = Conversions.convertParameterToPoints( tok.getParameter(), pdf );
-        if ( indent < 0 ) { //TODO: get column width and make it the maximum indent to test for
-            GDD gdd = pdf.getGdd();
-            gdd.logWarning( gdd.getLit( "LINE#" ) + ": " + tok.getSource().getLineNumber() + " " +
-                            gdd.getLit( "ERROR.INVALID_PARAGRAPH_INDENT" ) + " " +
-                            gdd.getLit( "IGNORED" ));
+        try {
+            newIndent = obtainNewIndent( tok, pdf );
+        }
+        catch( InvalidCommandParameterException icpe ) {
+            showErrorMsg( tok, pdf );
             return;
         }
 
-        float currFirstLineIndent = pdf.getFirstLineIndent();
-
-        if ( indent != currFirstLineIndent ) {
-            pdf.setFirstLineIndent( indent, tok.getSource() );
-            return;
+        if ( newIndent != pdf.getFirstLineIndent() ) {
+            pdf.setFirstLineIndent( newIndent, tok.getSource() );
         }
+    }
+
+    /**
+     * Extracts the new indent amount from the token and verifies its value.
+     * @param tok token containing the new indent specification
+     * @param pdf current PDF status info
+     * @throws org.pz.platypus.exceptions.InvalidCommandParameterException if indent value is not valid
+     * @return the indent if value is valid
+     */
+    float obtainNewIndent( final Token tok, final PdfData pdf )
+            throws InvalidCommandParameterException
+    {
+        float newIndent = Conversions.convertParameterToPoints( tok.getParameter(), pdf );
+
+        if ( newIndent < 0 || newIndent >= pdf.getColumnWidth() ) {
+            throw new InvalidCommandParameterException();
+        }
+
+        return( newIndent );
+    }
+
+    /**
+     * Show error message, giving location in Platypus input file
+     * @param tok contains the location data
+     * @param pdf contains the location of the logger and literals file
+     */
+    void showErrorMsg( final Token tok, final PdfData pdf )
+    {
+        GDD gdd = pdf.getGdd();
+        gdd.logWarning( gdd.getLit( "FILE#" ) + ": " + tok.getSource().getFileNumber() + " " +
+                        gdd.getLit( "LINE#" ) + ": " + tok.getSource().getLineNumber() + " " +
+                        gdd.getLit( "ERROR.INVALID_PARAGRAPH_INDENT" ) + " " +
+                        gdd.getLit( "IGNORED" ));
+
     }
 
     public String getRoot()
