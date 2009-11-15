bc060b549e785c0bf68c3bdd237c59f4cf9ce88e
binstock <binstock@dee37da4-d874-0410-bba0-bb64538a29bc>
2009-11-15 09:13:00 +0000

    Removed files that are no longer used in testing.

--- src/test/groovy/runTests.bat
+++
@@ -1,2 +0,0 @@
-rem runs the functional tests in scripts.txt with output going to the <project> jar directory
-groovy framework.groovy scripts.txt d:\dev\platypus\platypus\target\jar
--- src/test/groovy/test1.bat
+++
@@ -1,1 +0,0 @@
-java -jar D:\Dev\platypus\platypus\target\jar\Platypus.jar  D:\Dev\platypus\testdata\commandonly.platy d:\dev\platypus\platypus\target\jar\shouldBeEmpty.pdf
