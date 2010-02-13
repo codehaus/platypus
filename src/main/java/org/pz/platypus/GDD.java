a6dadba0d6bcd17b1ce19b6f1028059d3e71aed4
atulkhot <atulkhot@dee37da4-d874-0410-bba0-bb64538a29bc>
2010-02-13 18:08:42 +0000

    to test svn commit access

--- src/main/java/org/pz/platypus/GDD.java
+++ src/main/java/org/pz/platypus/GDD.java
@@ -19,6 +19,7 @@ import java.util.logging.*;
  * Global document data. Is a singleton, although not created as such due to testing repercussions.
  * 
  * @author alb
+ * @author atul
  */
 public class GDD 
 {
