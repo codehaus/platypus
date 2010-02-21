810ff105eb5d4faf6600782c4791f694d4794167
binstock <binstock@dee37da4-d874-0410-bba0-bb64538a29bc>
2010-02-21 15:10:53 +0000

    Implementing the format change required by a code listing.

--- src/main/java/org/pz/platypus/plugin/pdf/commands/PdfItalicsOff.java
+++ src/main/java/org/pz/platypus/plugin/pdf/commands/PdfItalicsOff.java
@@ -1,7 +1,7 @@
 /***
  *  Platypus: Page Layout and Typesetting Software (free at platypus.pz.org)
  *
- *  Platypus is (c) Copyright 2006-08 Pacific Data Works LLC. All Rights Reserved.
+ *  Platypus is (c) Copyright 2006-10 Pacific Data Works LLC. All Rights Reserved.
  *  Licensed under Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0.html)
  */
 
@@ -28,10 +28,12 @@ public class PdfItalicsOff implements OutputCommandable
             throw new IllegalArgumentException();
         }
 
-        PdfData pdf = (PdfData) context;
+        PdfData pdd = (PdfData) context;
 
-        PdfFont font = pdf.getFont();
-        font.setItalics( false, tok.getSource() );
+        PdfFont font = pdd.getFont();
+        if( font.getItalics() ) {
+            font.setItalics( false, tok.getSource() );
+        }
     }
 
     public String getRoot()
