d3a44a51b486937459d44f43beff36cb84f617b5
atulkhot <atulkhot@dee37da4-d874-0410-bba0-bb64538a29bc>
2010-03-18 03:46:43 +0000

    UtlCommand  refactoring to template method

--- trunk/src/main/java/org/pz/platypus/plugin/pdf/commands/PdfUrl.java
+++ trunk/src/main/java/org/pz/platypus/plugin/pdf/commands/PdfUrl.java
@@ -7,9 +7,8 @@
 
 package org.pz.platypus.plugin.pdf.commands;
 
+import org.pz.platypus.AbstractCommands.UrlCommand;
 import org.pz.platypus.Token;
-import org.pz.platypus.GDD;
-import org.pz.platypus.interfaces.OutputCommandable;
 import org.pz.platypus.interfaces.OutputContextable;
 import org.pz.platypus.plugin.pdf.PdfData;
 import org.pz.platypus.plugin.pdf.PdfOutfile;
@@ -19,59 +18,14 @@ import org.pz.platypus.plugin.pdf.PdfOutfile;
  *
  * @author alb
  */
-public class PdfUrl implements OutputCommandable
+// public class PdfUrl implements OutputCommandable
+public class PdfUrl extends UrlCommand
 {
-    private String root = "[url:";
 
-    public void process( final OutputContextable context, final Token tok, final int tokNum )
-    {
-        if( context == null || tok == null || tok.getParameter().getString() == null ) {
-            throw new IllegalArgumentException();
-        }
-
+    @Override
+    protected void outputUrl(final OutputContextable context, String url, String coverText) {
         PdfData pdf = (PdfData) context;
-        String urlParameter = tok.getParameter().getString();
-        String url;
-        String coverText = null;
-
-        // test for "|text: after URL, which would signal presence of cover text. If found,
-        // set url and coverText to the respective strings in urlParameter; else, it's all
-        // URL, so set url and leave coverText = null
-        int textFlag = urlParameter.indexOf( "|text:" );
-        if( textFlag > 0 ) {
-            coverText = urlParameter.substring( textFlag + "|text:".length() );
-            url = urlParameter.substring( 0, textFlag - 1);
-        }
-        else {
-            url = urlParameter;
-        }
-
-        if( url == null ) {
-            showErrorMsg( tok, pdf );
-            return;
-        }
-
         PdfOutfile outfile = pdf.getOutfile();
-        outfile.addUrl( url, coverText );
-    }
-
-    /**
-     * Show error message, giving location in Platypus input file
-     * @param tok contains the location data
-     * @param pdf contains the location of the logger and literals file
-     */
-    void showErrorMsg( final Token tok, final PdfData pdf )
-    {
-        GDD gdd = pdf.getGdd();
-        gdd.logWarning( gdd.getLit( "FILE#" ) + ": " + tok.getSource().getFileNumber() + " " +
-                        gdd.getLit( "LINE#" ) + ": " + tok.getSource().getLineNumber() + " " +
-                        gdd.getLit( "ERROR.URL_IS_NULL" ) + " " +
-                        gdd.getLit( "IGNORED" ));
-
-    }
-
-    public String getRoot()
-    {
-        return( root );
+        outfile.addUrl( url, coverText );        
     }
 }
