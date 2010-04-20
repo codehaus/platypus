a73cb0321ebad8c019a4e5e0b30e9511b7039bc3
binstock <binstock@dee37da4-d874-0410-bba0-bb64538a29bc>
2010-04-20 04:34:33 +0000

    Finished implementation of code listings, using the new abstract/concrete command (CodeWithOptions).
    Now, only needs tests and description in the documentation for this release.

--- src/main/java/org/pz/platypus/interfaces/OutputContextable.java
+++ src/main/java/org/pz/platypus/interfaces/OutputContextable.java
@@ -1,20 +1,26 @@
 /***
  *  Platypus: Page Layout and Typesetting Software (free at platypus.pz.org)
  *
- *  Platypus is (c) Copyright 2006-08 Pacific Data Works LLC. All Rights Reserved.
+ *  Platypus is (c) Copyright 2010 Pacific Data Works LLC. All Rights Reserved.
  *  Licensed under Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0.html)
  */
 
 package org.pz.platypus.interfaces;
 
 import org.pz.platypus.GDD;
+import org.pz.platypus.Source;
 
 /**
- * Very generic interface for Output context info
+ * Interface for Output context info
  *
  * @author alb
  */
 public interface OutputContextable
 {
-    public GDD getGdd();    
+    public GDD getGdd();
+
+    boolean inCodeSection();
+
+    void    setLineNumberLast( int lineNum, Source s);
+    void    setLineNumberSkip( int skipSize, Source s );
 }
