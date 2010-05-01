79a91e7697582d4d915c26c92d0c344eb503f306
binstock <binstock@dee37da4-d874-0410-bba0-bb64538a29bc>
2010-05-01 07:47:47 +0000

    Added htmlTest.groovy to be used as a template for testing HTML files

---
+++ src/test/groovy/functionaltests/htmlTest.groovy
@@ -0,0 +1,92 @@
+/***
+ *  Platypus: Page Layout and Typesetting Software (free at platypus.pz.org)
+ *
+ *  Platypus is (c) Copyright 2010 Pacific Data Works LLC. All Rights Reserved.
+ *  Licensed under Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0.html)
+ *
+ *  Runs functional tests on the HTML plugin.
+ *
+ *  @author alb
+ */
+
+ //-------------------- start of main line ------------------------//
+
+// the JAR to test should be passed to this script as args[0]
+if( ! args ) {
+    println( "Error! Missing arg.\n" +
+             "Usage: groovy htmlTest.groovy JAR-file-to-test-with-path.\n" +
+             "Please rerun specifying JAR to test (with full path)")
+    return;
+}
+
+// create the command line for running Platypus
+def String jarUnderTest = args[0]
+def String javaRun = "java -jar " + jarUnderTest
+
+// run the test(s)
+testItalics( javaRun ) 
+
+ 
+return
+
+//----------------------- end of main line -----------------------//
+
+/**
+ * Test whether simple italics work correctly in the HTML plugin
+ */
+
+def void testItalics( String javaRun )
+{
+  def String description =
+      "Test: Simple italics."
+
+      // create a Platypus file containing italicized text.
+      def String testFileName = "testItalics.plat"
+      def File testFile = new File( testFileName )
+      PrintWriter pw = new PrintWriter( testFile )
+      pw.write(
+          "Atul [+i]rocks[-i]!\n" 
+      );
+      pw.close()
+	  
+	  // Make sure we have successfully created the input file
+      if ( ! testFile.exists() ) {
+          println( "***FAILURE in ${description}. Could not create test file" )
+          return
+      }
+
+    // run Platypus and capture for error message as well as a generated HTML file
+    String commandLine =  javaRun + " ${testFileName} ${testFileName}.html "
+    def proc = commandLine.execute()
+    def err = proc.err.text
+
+	// did we create an output file? If not, report error.
+    def File htmlFile = new File( "${testFileName}.html" )
+	if( ! htmlFile.exists() ) {
+		println( "***FAILURE in " + description + " No HTML file created." )
+		return;
+	}
+	
+	// did Platypus report any errors?
+    if( err != null && err != "" ){
+        println( "***FAILURE in " + description + "Platypus reported an error: " +  err )
+		testFile.delete()
+		htmlFile.delete()
+    }
+
+	// read contents of output file into a string	
+	String html = htmlFile.getText()
+	
+	// test for the expected output. If it doesn't match, fail and show the invalid HTML
+	if( html.contains( "Atul <I>rocks</I>!" )) {
+		println( "Success in " + description )
+	}
+	else {
+		println( "***FAILURE in " + description + ": Generated HTML not as expected." )
+		println( "        -> " + html )
+	}
+	
+	// delete the test Platypus file and the generated HTML file.
+    testFile.delete()
+    htmlFile.delete()
+}
