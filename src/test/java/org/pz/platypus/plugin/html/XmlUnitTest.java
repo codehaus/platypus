00f47ae18ad2990f7b8a9a372070d9501b5e4e1a
atulkhot <atulkhot@dee37da4-d874-0410-bba0-bb64538a29bc>
2010-08-19 09:22:26 +0000

    Forgot to put in the xmlunit lib - so the compilation broke on the bamboo server... will put back the code tonight - along with the lib

---
+++ src/test/java/org/pz/platypus/plugin/html/XmlUnitTest.java
@@ -0,0 +1,28 @@
+package org.pz.platypus.plugin.html;
+
+
+import org.pz.platypus.interfaces.OutputCommandable;
+import org.pz.platypus.plugin.html.commands.HtmlBoldOff;
+import org.pz.platypus.plugin.html.commands.HtmlItalicsOff;
+import org.pz.platypus.plugin.html.commands.HtmlItalicsOn;
+
+
+/**
+ * Platypus: Page Layout and Typesetting Software (free at platypus.pz.org)
+ * <p/>
+ * Platypus is (c) Copyright 2006-08 Pacific Data Works LLC. All Rights Reserved.
+ * Licensed under Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0.html)
+ * @author: atul
+ *
+ */
+public class XmlUnitTest /* extends XMLTestCasxe */ {
+
+    public void testForEquality() throws Exception {
+        String myControlXML = "<msg><uuid>0x00435A8C</uuid></msg>";
+        String myTestXML = "<msg><localId>2376</localId></msg>";
+//        assertXMLEqual("Comparing test xml to control xml",
+//                       myControlXML, myTestXML);
+    }
+    
+
+}
