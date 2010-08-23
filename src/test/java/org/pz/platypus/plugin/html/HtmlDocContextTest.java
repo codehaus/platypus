20b672973daa9b1e0d6c7ef75afbecfd35fdc060
atulkhot <atulkhot@dee37da4-d874-0410-bba0-bb64538a29bc>
2010-08-23 06:07:06 +0000

    testbed for the html plugin

---
+++ src/test/java/org/pz/platypus/plugin/html/HtmlDocContextTest.java
@@ -0,0 +1,38 @@
+package org.pz.platypus.plugin.html;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.framework.TestCase;
+
+/**
+ * HtmlDocContext Tester.
+ *
+ * @author <Authors name>
+ * @since <pre>08/23/2010</pre>
+ * @version 1.0
+ */
+public class HtmlDocContextTest extends TestCase {
+    public HtmlDocContextTest(String name) {
+        super(name);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testGetNestedTagCmds() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    public void testSetGetOutfile() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    public static Test suite() {
+        return new TestSuite(HtmlDocContextTest.class);
+    }
+}
