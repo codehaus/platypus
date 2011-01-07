752e7f5ee57a31085f9e160b15bcc4aa5446c9fd
binstock <binstock@dee37da4-d874-0410-bba0-bb64538a29bc>
2011-01-07 09:39:02 +0000

    Corrected small error in PdfHardCR.java and then wrote long overdue unit tests for the class.

--- src/main/java/org/pz/platypus/plugin/pdf/commands/PdfHardCR.java
+++ src/main/java/org/pz/platypus/plugin/pdf/commands/PdfHardCR.java
@@ -1,7 +1,7 @@
 /***
  *  Platypus: Page Layout and Typesetting Software (free at platypus.pz.org)
  *
- *  Platypus is (c) Copyright 2006-09 Pacific Data Works LLC. All Rights Reserved.
+ *  Platypus is (c) Copyright 2011 Pacific Data Works LLC. All Rights Reserved.
  *  Licensed under Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0.html)
  */
 
@@ -80,7 +80,7 @@ public class PdfHardCR implements IOutputCommand
             return( false );
         }
 
-        if( ! prevTok.getRoot().equals( "[cr]" )) {
+        if(( prevTok.getRoot() == null ) || ! prevTok.getRoot().equals( "[cr]" )) {
             return( false );
         }
 
